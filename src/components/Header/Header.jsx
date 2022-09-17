93ec046d327c1ff8ac7b3e12ca1c558547f44ae1
truc2203 <tranthanhtruc22031998@gmail.com>
2022-09-17 14:23:54 +0700

    fix navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import {FcPhotoReel} from 'react-icons/fc'
+import { useState } from "react";
 import {FaUserCircle} from 'react-icons/fa'
 import { useNavigate } from "react-router-dom";
 import { notification } from "antd";
 const Header = () => {
+  const [open,setOpen] = useState(false)
   const navigate = useNavigate()
   const movePath = (path) => {
     navigate(`${path}`)
   }
   const userData = JSON.parse(localStorage.getItem("user"));  
-
+  const showNavbar = () => {
+    setOpen(!open)
+  }
   const hanldeLogout = () => {
     localStorage.removeItem('user')
     movePath('/')
@@ -23,8 +27,30 @@ const Header = () => {
     <button onClick={() => movePath('/')} className="navbar-brand" >
       <FcPhotoReel/>
     </button>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button onClick={() => showNavbar()} style={{zIndex:'999'}} className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
+      <ul style={{display : open ? 'block' : 'none'}} className="navbar-nav showNav">
+        <li className="nav-item px-3">
+          <button onClick={() => movePath('/')} className="nav-link" aria-current="page" >Lịch Chiếu</button>
+        </li>
+        <li className="nav-item px-3">
+          <button onClick={() => movePath('/')} className="nav-link" >Cụm rạp</button>
+        </li>
+        <li className="nav-item px-3">
+          <button onClick={() => movePath('/')} className="nav-link" >Tin Tức</button>
+        </li>
+        <li className="nav-item px-3">
+          <button onClick={() => movePath('/')}  className="nav-link" >Ứng Dụng</button>
+        </li>
+        <li className="nav-item px-3">
+          <button onClick={() => movePath('register')}  className="nav-link " >Đăng Ký</button>
+        </li>
+        <li className="nav-item px-3">
+          <button onClick={() => movePath(userData?.taiKhoan ? '' : 'login')} className="nav-link d-inline" >{userData?.taiKhoan ? `${userData.taiKhoan}` :'Đăng nhập'}</button>
+          <button onClick={() => hanldeLogout()} className="nav-link d-inline " >{userData?.taiKhoan ? '| Đăng Xuất' :''}</button>
+          
+        </li>
+      </ul>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
